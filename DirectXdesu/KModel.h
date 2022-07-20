a582395f740d06a2a13dbe62ded158b9453b581a
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-07-20 14:34:25 +0900

    移行

--- DirectXdesu/KModel.h
+++ DirectXdesu/KModel.h
@@ -1,5 +1,19 @@
 #pragma once
-class KModel
-{
+#include <vector>
+#include "KVertex.h"
+
+class KModel : public KVertex {
+public:
+	// 頂点データ
+	std::vector<Vertex> vertices;
+	// インデックスデータ
+	std::vector<short> indices;
+	// modelusakusei
+	void CreateModel(ID3D12Device* dev);
 };
 
+class Cube : public KModel {
+public:
+	Cube();
+	~Cube() {};
+};

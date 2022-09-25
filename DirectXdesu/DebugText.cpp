edb7ad6e3e68e98d2ee68f034256ca6dbbdd9001
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-09-25 15:07:28 +0900

    デバッグテキスト途中

---
+++ DirectXdesu/DebugText.cpp
@@ -0,0 +1,1 @@
+#include "DebugText.h"
---
+++ DirectXdesu/DebugText.h
@@ -0,0 +1,15 @@
+#pragma once
+class DebugText
+{
+public:
+	// ’è”‚ÌéŒ¾
+	static const int maxCharCount = 256;
+	static const int fontWidth = 9;
+	static const int fontHeight = 18;
+	static const int fontLineCount = 14;
+	// ƒƒ“ƒoŠÖ”‚ÌéŒ¾
+
+private:
+
+};
+

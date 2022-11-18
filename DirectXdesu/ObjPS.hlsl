2f66ac698d9dbf57a6a78f3c0b6bd86e219050b2
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-11-18 13:09:36 +0900

    objマテリアル読み込みたい

--- DirectXdesu/ObjPS.hlsl
+++ DirectXdesu/ObjPS.hlsl
@@ -1,4 +1,4 @@
-#include "ObjShaderHeader.hlsli"
+#include "Obj.hlsli"
 
 Texture2D<float4> tex : register(t0);
 SamplerState smp : register(s0);
--- DirectXdesu/ObjVS.hlsl
+++ DirectXdesu/ObjVS.hlsl
@@ -1,4 +1,4 @@
-#include "ObjShaderHeader.hlsli"
+#include "Obj.hlsli"
 
 VSOutput main(float4 pos : POSITION, float3 normal : NORMAL, float2 uv : TEXCOORD)
 {

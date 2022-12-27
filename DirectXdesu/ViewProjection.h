f6b2487e773a35306ff2e6ae0dad92d702af409d
IijimaKaoru <k021g1465@g.neec.ac.jp>
2022-12-27 17:35:44 +0900

    意味ない関数消去

--- DirectXdesu/ViewProjection.h
+++ DirectXdesu/ViewProjection.h
@@ -13,15 +13,15 @@ public:
 	void Update(int width, int height);
 
 	// カメラの距離
-	float lenZ;
-	float angleX; // カメラの回転角X
-	float angleY; // カメラの回転角Y
+	float lenZ = 0;
+	float angleX = 0; // カメラの回転角X
+	float angleY = 0; // カメラの回転角Y
 	// ビュー変換行列
-	XMMATRIX matView;
-	XMFLOAT3 eye;
-	XMFLOAT3 target;
-	XMFLOAT3 up;
+	XMMATRIX matView{};
+	XMFLOAT3 eye{};
+	XMFLOAT3 target{};
+	XMFLOAT3 up{};
 	// 射影変換行列の計算
-	XMMATRIX matProjection;
+	XMMATRIX matProjection{};
 };
 

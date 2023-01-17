a4ea6804db8515cbfa7ba57b9c7a54f7d50132e5
IijimaKaoru <k021g1465@g.neec.ac.jp>
2023-01-17 09:24:33 +0900

    hoge2

--- DirectXdesu/main.cpp
+++ DirectXdesu/main.cpp
@@ -225,7 +225,8 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 #pragma endregion
 
 	KObject3d hoge;
-	
+	hoge.Initialize();
+	hoge.transform.scale = { 10,10,10 };
 
 	hoge.LoadModel(&tekitou);
 

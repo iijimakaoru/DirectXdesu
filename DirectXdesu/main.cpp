f1ca7a142c12ddedc1b0b4b24fd595b1c25de823
IijimaKaoru <k021g1465@g.neec.ac.jp>
2023-01-30 16:01:42 +0900

    main整理

--- DirectXdesu/main.cpp
+++ DirectXdesu/main.cpp
@@ -3,36 +3,13 @@
 #include "KWinApp.h"
 #include "KDirectXCommon.h"
 #include "KInput.h"
-#include "KDepth.h"
-#include "KTexture.h"
-#include "KObject3d.h"
-#include "ViewProjection.h"
-#include "KModel.h"
-#include "KShader.h"
 #ifdef _DEBUG
 #include <iostream>
 #endif
 #pragma comment(lib, "d3dcompiler.Lib")
 #include "Sound.h"
 #include <d3dx12.h>
-#include "ConstBuffer.h"
-#include "PipelineSet.h"
-#include "Vector4.h"
-#include "Sprite.h"
-#include "DebugText.h"
-
-#include <array>
-
-#include"Player.h"
-#include"Stage.h"
-#include"Boss.h"
-#include "DebugCamera.h"
-#include "Camera.h"
-#include "MyMath.h"
-#include "ParticleManager.h"
 #include "GameScence.h"
-
-#include "KGPlin.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {

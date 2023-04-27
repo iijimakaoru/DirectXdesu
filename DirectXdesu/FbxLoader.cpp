e1b77baf28ffadb3b2e3619e34630bba93c8fc29
IijimaKaoru <k021g1465@g.neec.ac.jp>
2023-04-27 10:19:53 +0900

    01_07完了

--- DirectXdesu/FbxLoader.cpp
+++ DirectXdesu/FbxLoader.cpp
@@ -49,4 +49,54 @@ void FbxLoader::LoadModelFromFile(const string& modelName)
     FbxScene* fbxScene = FbxScene::Create(fbxManager, "fbxScene");
     // ファイルから読み込んだfbx情報をシーンにインポート
     fbxImporter->Import(fbxScene);
+    // モデル作成
+    FbxModel* fbxModel = new FbxModel();
+    fbxModel->name = modelName;
+    // Fbxノードの数を取得
+    int nodeCount = fbxScene->GetNodeCount();
+    // あらかじめ必要分のメモリを確保することで、アドレスがずれるのを予防
+    fbxModel->nodes.reserve(nodeCount);
+    // ルートノードから順に解析してモデルに流し込む
+    ParseNodeRecursive(fbxModel, fbxScene->GetRootNode());
+    // Fbxシーン解放
+    fbxScene->Destroy();
+}
+
+void FbxLoader::ParseNodeRecursive(FbxModel* model, FbxNode* fbxNode, Node* parent)
+{
+    // ノード名を取得
+    string name = fbxNode->GetName();
+    // モデルにノードを追加
+    model->nodes.emplace_back();
+    Node& node = model->nodes.back();
+    // Fbxノードの情報を解析してノードに記録
+    node.name = fbxNode->GetName();
+    // Fbxノードのメッシュ情報を解析
+    FbxDouble3 rotation = fbxNode->LclRotation.Get();
+    FbxDouble3 scaling = fbxNode->LclScaling.Get();
+    FbxDouble3 translation = fbxNode->LclTranslation.Get();
+    // 形式変換して代入
+    node.rotation = { (float)rotation[0],(float)rotation[1],(float)rotation[2],0.0f };
+    node.scaling = { (float)scaling[0],(float)scaling[1],(float)scaling[2],0.0f };
+    node.translation = { (float)translation[0],(float)translation[1],(float)translation[2],1.0f };
+    // 回転角をDegree(度)からラジアンに変換
+    node.rotation.m128_f32[0] = XMConvertToRadians(node.rotation.m128_f32[0]);
+    node.rotation.m128_f32[1] = XMConvertToRadians(node.rotation.m128_f32[1]);
+    node.rotation.m128_f32[2] = XMConvertToRadians(node.rotation.m128_f32[2]);
+    // スケール、回転、平行移動行列の計算
+    XMMATRIX matScaling, matRotation, matTransition;
+    matScaling = XMMatrixScalingFromVector(node.scaling);
+    matRotation = XMMatrixRotationRollPitchYawFromVector(node.rotation);
+    matTransition = XMMatrixTranslationFromVector(node.translation);
+    // ローカル変形行列の計算
+    node.transform = XMMatrixIdentity();
+    node.transform *= matScaling; // スケーリング
+    node.transform *= matRotation; // 回転
+    node.transform *= matTransition; // 平行移動
+
+    // 子ノードに対して再帰呼び出し
+    for (int i = 0; i < fbxNode->GetChildCount(); i++)
+    {
+        ParseNodeRecursive(model, fbxNode->GetChild(i), &node);
+    }
 }
--- DirectXdesu/FbxLoader.h
+++ DirectXdesu/FbxLoader.h
@@ -7,15 +7,19 @@
 
 #include <string>
 
+#include "FbxModel.h"
+
+using namespace DirectX;
+
 class FbxLoader
 {
 private:// エイリアス
 	// stdを省略
 	using string = std::string;
 
-/// <summary>
-/// メンバ関数
-/// </summary>
+	/// <summary>
+	/// メンバ関数
+	/// </summary>
 public:
 	/// <summary>
 	/// シングルトンインスタンスの取得
@@ -33,10 +37,17 @@ public:
 	/// </summary>
 	void Finalize();
 
-/// <summary>
-/// Fbxモデル読み込み
-/// </summary>
+	/// <summary>
+	/// Fbxモデル読み込み
+	/// </summary>
 	void LoadModelFromFile(const string& modelName);
+
+	/// <summary>
+	/// 再帰的にノード構成を解析
+	/// </summary>
+	/// <param name = "model">読み込み先モデルオブジェクト</param>
+	/// <param name = "fbxNode">解析対象のノード</param>
+	void ParseNodeRecursive(FbxModel* model, FbxNode* fbxNode, Node* parent = nullptr);
 
 private:
 	// privateなコンストラクタ（シングルトンパターン）
@@ -48,9 +59,9 @@ private:
 	// コピー代入演算子を禁止（シングルトンパターン）
 	void operator=(const FbxLoader& obj) = delete;
 
-/// <summary>
-/// メンバ変数
-/// </summary>
+	/// <summary>
+	/// メンバ変数
+	/// </summary>
 private:
 	// Fbxマネージャー
 	FbxManager* fbxManager = nullptr;

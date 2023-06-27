ede5cfbd067a9bd29b84326672f3892409bd4e7f
IijimaKaoru <k021g1465@g.neec.ac.jp>
2023-06-27 13:58:43 +0900

    弾追加

---
+++ DirectXdesu/Application/Bullet.cpp
@@ -0,0 +1,13 @@
+#include "Bullet.h"
+
+void Bullet::Init()
+{
+}
+
+void Bullet::Update(ViewProjection& viewPro)
+{
+}
+
+void Bullet::Draw()
+{
+}
--- DirectXdesu/Application/Bullet.h
+++ DirectXdesu/Application/Bullet.h
@@ -1,1 +1,40 @@
 #pragma once
+#include <memory>
+#include "KTexture.h"
+#include "KObject3d.h"
+#include "KModel.h"
+
+class Bullet
+{
+public:
+	// 初期化
+	void Init();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	/// <param name="viewPro"></param>
+	void Update(ViewProjection& viewPro);
+
+	// 描画
+	void Draw();
+
+private:
+	// オブジェクト
+	std::unique_ptr<KObject3d> object3d;
+
+	// モデル
+	std::unique_ptr<KModel> model;
+
+	// テクスチャ
+	KTexture tex;
+
+	// パイプライン
+	std::unique_ptr<KGPlin> pipeline;
+
+	// シェーダー
+	KShader shader;
+
+	// スピード
+	float speed = 1.0f;
+};

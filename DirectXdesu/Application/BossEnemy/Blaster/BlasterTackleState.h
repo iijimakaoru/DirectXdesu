7a996c5680f26ba0bf198416bafcf23a812306d4
IijimaKaoru <k021g1465@g.neec.ac.jp>
2024-01-31 16:27:14 +0900

    タックル攻撃コードリファクタリング

--- DirectXdesu/Application/BossEnemy/Blaster/BlasterTackleState.h
+++ DirectXdesu/Application/BossEnemy/Blaster/BlasterTackleState.h
@@ -8,14 +8,33 @@ public:
 
 	void Update() override;
 
-	private:
+	enum Phase {
+		CubeClose,
+		CubeOpen,
+		Aim, StandTackle,
+		GoTackle,
+		BackPos,
+		End
+	};
+
+private:
 	void TackleVec();
 	void Tackle();
+
+	void CubeCloseAct();
+	void CubeOpenAct();
+	void AimAct();
+	void StandTackleAct();
+	void GoTackleAct();
+	void BackPosAct();
+	void EndAct();
 
 private:
 	float angle = 0;
 	float startTimer = 0;
 	float startTime = 0;
+
+	Phase actsPhase;
 
 	KMyMath::Vector3 tackleVec;
 };

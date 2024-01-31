426cbb0868b4a2d820a0e93b90f28d8d4e789aa2
IijimaKaoru <k021g1465@g.neec.ac.jp>
2024-01-31 16:49:25 +0900

    ボスのノーマル状態リファクタリング

--- DirectXdesu/Application/BossEnemy/Blaster/BlasterStandState.h
+++ DirectXdesu/Application/BossEnemy/Blaster/BlasterStandState.h
@@ -2,8 +2,7 @@
 #include "ActState.h"
 #include "MyMath.h"
 
-class BlasterStandState : public ActState
-{
+class BlasterStandState : public ActState {
 public:
 	// コンストラクタ
 	BlasterStandState();
@@ -11,9 +10,17 @@ public:
 	// 更新
 	void Update() override;
 
+	enum Phase { CubeClose, Stand, End };
+
+private:
+	void CubeCloseAct();
+	void StandAct();
+	void EndAct();
+
 private:
 	float angle = 0;
 	float startTimer = 0;
 	float startTime = 0;
-};
 
+	Phase actsPhase;
+};

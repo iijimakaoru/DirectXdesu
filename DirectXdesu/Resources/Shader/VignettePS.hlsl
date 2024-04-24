4cdf72c872346ba2e255f5b0bcdcdc0c681bddc5
IijimaKaoru <k021g1465@g.neec.ac.jp>
2024-04-24 12:22:00 +0900

    Vignetteの色が変えれた。なんで？

--- DirectXdesu/Resources/Shader/VignettePS.hlsl
+++ DirectXdesu/Resources/Shader/VignettePS.hlsl
@@ -10,9 +10,9 @@ float4 main(VSOutput input) : SV_TARGET
     
     //‰~‚Ì”ÍˆÍ‚ðŒvŽZ
     float vignette = length(float2(0.5,0.5) - input.uv);
-    vignette = clamp(vignette - 0.2, 0, 1);
-
-    textureColor -= (vignette);
+    vignette = clamp(vignette - 0.4, 0, 1);
+    
+    textureColor.rgb += (vignette) * float3(1, 0, 0);
 
     return float4(textureColor.rgb, 1.0);
 }

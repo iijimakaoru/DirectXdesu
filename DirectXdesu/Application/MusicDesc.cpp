01f02223ca1e0ade26d1d73b253a20d7c243bddf
IijimaKaoru <k021g1465@g.neec.ac.jp>
2025-01-10 16:39:45 +0900

    みやうちエンジンからMusicDescクラス移植

---
+++ DirectXdesu/Application/MusicDesc.cpp
@@ -0,0 +1,71 @@
+#include "MusicDesc.h"
+
+bool Comparison(const Beat& now,const Beat& change) {
+	if ( now.measure < change.measure )
+	{
+		return true;
+	}
+	if ( now.beat<change.beat )
+	{
+		return true;
+	}
+	return false;
+}
+
+MusicDesc::MusicDesc()
+{
+	bpm_ = 120;
+	meter_.beatAmount = 4;
+	meter_.beatLength = 4;
+}
+
+MusicDesc::MusicDesc(float bpm,Meter meter)
+{
+	bpm_ = bpm;
+	meter_ = meter;
+}
+
+MusicDesc::MusicDesc(float bpm)
+{
+	bpm_ = bpm;
+}
+
+MusicDesc::~MusicDesc()
+{
+}
+
+float MusicDesc::ConvertBeatToMiliSeconds(const Beat& beat)
+{
+	float beatTime = 0;
+	Beat now = { 0,0,1 };
+	for (const Beat& change:changeList )
+	{
+		if ( Comparison(beat,change) )
+		{
+			break;
+		}
+	}
+	//全音符の長さを得る（ミリ秒）
+	float whole = ( ( 60 / bpm_ ) * 1000.0f ) * 4.0f;
+	//1小節の長さを得る
+	float measure = ( whole / meter_.beatLength ) * meter_.beatAmount;
+	//小節分の時間を加算する
+	beatTime += measure * beat.measure;
+	//拍数分の時間を加算する
+	beatTime += whole * ( static_cast< float >( beat.beat ) / beat.LPB );
+
+	return beatTime;
+}
+
+float MusicDesc::GetNoteLength(const Beat& beat)
+{
+	beat.beat;
+	float whole = ( ( 60 / bpm_ ) * 1000.0f ) * 4.0f;
+	return whole;
+}
+
+Meter MusicDesc::GetMeter(const Beat& beat)
+{
+	beat;
+	return Meter();
+}
---
+++ DirectXdesu/Application/MusicDesc.h
@@ -0,0 +1,58 @@
+#pragma once
+#include<map>
+#include<list>
+struct Beat
+{
+	int measure = 0;//小節番号
+	int beat = 0;//拍数
+	float LPB = 4;//1拍の長さ
+};
+
+struct Meter
+{
+	int beatAmount = 4;//1小節に入る拍の数
+	int beatLength = 4;//1拍の長さ
+};
+enum DIRECTION
+{
+	right, //右
+	left,  //左
+	up,	   //上
+	dawn   //下
+};
+
+
+struct Note
+{
+	Beat beat;//何拍目の音
+	int lane = 0;//レーン指定
+	DIRECTION direction=right;//方向
+	bool isHit = false;
+};
+
+class MusicDesc
+{
+public:
+	MusicDesc();//コンストラクタ
+	MusicDesc(float bpm,Meter meter);//コンストラクタ
+	MusicDesc(float bpm);//コンストラクタ
+	~MusicDesc();//デストラクタ
+
+	float ConvertBeatToMiliSeconds(const Beat& beat);
+private:
+	float GetNoteLength(const Beat& beat);
+	Meter GetMeter(const Beat& beat);
+
+private:
+	std::map<Beat,float>bpmChange_;
+	std::map<Beat,Meter>meterChange_;
+	std::list<Beat>changeList;
+	float bpm_;
+	float baseBPM = 120.0f;
+	Meter meter_;
+	Meter baseMeter;
+
+
+
+};
+

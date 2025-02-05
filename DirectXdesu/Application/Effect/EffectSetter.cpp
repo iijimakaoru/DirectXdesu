9ffb551d3f60f25ccbc1e629373ddf137194a655
IijimaKaoru <k021g1465@g.neec.ac.jp>
2025-02-05 11:42:21 +0900

    listに変更してみたけどゲーム内の重さは解決したけど生成される瞬間に一瞬だけ止まる問題がある

--- DirectXdesu/Application/Effect/EffectSetter.cpp
+++ DirectXdesu/Application/Effect/EffectSetter.cpp
@@ -49,7 +49,9 @@ void EffectSetter::Draw(const Timer& timer, const KMyMath::Matrix4& matView, con
 
 void EffectSetter::DeleteEffect()
 {
-	
+	arrowEffect_.remove_if([](std::unique_ptr<ArrowEffect>& arrowEffect) { return arrowEffect->GetIsDead(); });
+
+	groundEffect_.remove_if([](std::unique_ptr<GroundEffect>& groundEffect) { return groundEffect->GetIsDead(); });
 }
 
 void EffectSetter::SetArrowEffect(KMyMath::Vector3& pos, KMyMath::Vector3& rotation, KMyMath::Vector3& scale, KMyMath::Vector4& color,
@@ -81,4 +83,10 @@ void EffectSetter::SetGroundEffect(KMyMath::Vector3& pos, KMyMath::Vector3& rota
 			break;
 		}
 	}*/
+
+	std::unique_ptr<GroundEffect> newGroundEffect = std::make_unique<GroundEffect>();
+	newGroundEffect->Init(groundModel_.get(), timer, matView, matProjection);
+	newGroundEffect->SetParticle(pos, rotation, scale, color, timer, matView, matProjection);
+
+	groundEffect_.push_back(std::move(newGroundEffect));
 }

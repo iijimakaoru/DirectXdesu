dd77796a7bb97de4c308f3912fd633634da453e2
maple-c-blossom <90586502+maple-c-blossom@users.noreply.github.com>
2025-02-09 17:58:27 +0900

    push

--- External/motionLib/Source/Animation/Skelton.cpp
+++ External/motionLib/Source/Animation/Skelton.cpp
@@ -128,10 +128,6 @@ void MCBM::Skelton::CaptureBoneUpdate(YOLO_POSE_INDEX rootBoneName, uint32_t bon
 
 			nowBone.V3Norm();
 
-			nowBone.z += 0.025f;
-
-			nowBone.V3Norm();
-
 			if (i != 0)
 			{
 				initializeBone = tempVec;

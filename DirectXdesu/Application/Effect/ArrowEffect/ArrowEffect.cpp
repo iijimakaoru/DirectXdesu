d0b14a03fd89f7e7a916803f2e8e35445e9ab1e5
iijimakaoru <k021g1465@g.neec.ac.jp>
2025-02-12 22:34:29 +0900

    取り敢えず1.5倍時間増やした

--- DirectXdesu/Application/Effect/ArrowEffect/ArrowEffect.cpp
+++ DirectXdesu/Application/Effect/ArrowEffect/ArrowEffect.cpp
@@ -3,7 +3,7 @@
 
 void ArrowEffect::Init(MeshModel* model, const Timer* timer, const KMyMath::Matrix4& matView, const KMyMath::Matrix4& matProjection)
 {
-	lifeLimit_ = 20.0f;
+	lifeLimit_ = 30.0f;
 	meshModel_ = model;
 	isDead = true;
 	emitter_ = std::make_unique<Emitter>(100, 1, 1.0f, 2.5f, 0.5f,
